6747f1bc39f69a82c5d6d5ee2e21f9627e0eb853
Caspar Clemens Mierau <ccm@screenage.de>
2014-02-26 12:45:33 +0100

    making sure, changes are pushed

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 # make sure, we are in the right directory
 cd $(dirname "$0")
 
-rsync -e "ssh -p 22222" -avz -f "- deploy.sh" -f"- .git*" -f"+ *" --delete ./ greencart@212.87.44.119:~/html/
+git push origin master && rsync -e "ssh -p 22222" -avz -f "- deploy.sh" -f"- .git*" -f"+ *" --delete ./ greencart@212.87.44.119:~/html/
